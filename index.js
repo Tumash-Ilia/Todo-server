bb33d6c8265cf18362f94dfb347fa6f6fff214fa
Ilia_Tumash <ilia.tumash@tul.cz>
2023-01-05 19:41:46 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config();
 const app = express()
 const PORT = process.env.PORT || 8000
+app.use(cors())
+
 
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.route'))
